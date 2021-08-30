e13c51d61a600c7a0271c541b38b223d6eb7383d
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-08-30 22:32:10 +0300

    delicense-web/backend > dev: logic

---
+++ packages/delicense-web/backend/source/logic/index.ts
@@ -0,0 +1,13 @@
+// #region imports
+    // #region internal
+    import Delicensor from './Delicensor';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region exports
+export default {
+    Delicensor,
+};
+// #endregion exports
